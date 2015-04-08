a2cb06fb6cacc9edc2ea701600212eff1a7bfb95
Arinde Eniola <eniola.arinde@andela.co>
2015-04-08 12:14:19 +0100

    initial commit

---
+++ config/postgres.js
@@ -0,0 +1,11 @@
+var config = require('./config'),
+    knex = require('knex');
+
+module.exports = function() {
+	var db = knex({
+        client: 'pg',
+        connection: config.db  
+	});
+
+	return db;
+}
