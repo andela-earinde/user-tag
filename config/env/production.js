bd9f0a0758b4e1066d36ff1ffb7ea9504f25df56
Arinde Eniola <eniola.arinde@andela.co>
2015-04-13 00:49:52 +0100

    deploying to heroku

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
 	db: {
-			host: 'postgres://sizxcrjfkwzsrx:vB6v9lRAHVpoRcPxJr7K7jMln6@ec2-54-163-239-102.compute-1.amazonaws.com:5432/d9p0obfiuhc61l',
+			host: 'ec2-54-163-239-102.compute-1.amazonaws.com',
 			port: '5432',
 			user: 'sizxcrjfkwzsrx',
 			password: 'vB6v9lRAHVpoRcPxJr7K7jMln6',
