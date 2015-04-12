aba1f87ce67b0fa89eaa964eedddbe18d3cab149
Arinde Eniola <eniola.arinde@andela.co>
2015-04-12 22:39:44 +0100

    implemanted the the token and completed the test

---
+++ config/env/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+	db: {
+		client: 'pg',
+		connection: {
+			host: 'postgres://sizxcrjfkwzsrx:vB6v9lRAHVpoRcPxJr7K7jMln6@ec2-54-163-239-102.compute-1.amazonaws.com:5432/d9p0obfiuhc61l',
+			user: 'sizxcrjfkwzsrx',
+			password: 'vB6v9lRAHVpoRcPxJr7K7jMln6',
+			database: '	d9p0obfiuhc61'
+		}
+	},
+
+	name: 'heroku-postgres-ff934065'
+}
